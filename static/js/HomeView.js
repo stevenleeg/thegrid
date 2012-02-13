e98a5c22eee6c9624fcdb03ec36628d438e885ae
Steve Gattuso <stevenleeg@gmail.com>
2012-02-13 13:54:20 -0500

    Room existence is now checked as you type!

--- static/js/HomeView.js
+++ static/js/HomeView.js
@@ -1,7 +1,8 @@
 var HomeView = (function() {
 	var tpl = "home.html"
 	var load_timeout;
-	// Private
+	var check_timeout;
+
 	function roomEnterCb(data) {
 		BaseUI.done();
 		if(data['status'] == 200) {
@@ -11,9 +12,9 @@ var HomeView = (function() {
 		}
 	}
 	
-	// Public
 	function onLoad(pass) {
-		$("input[name=enter]").bind("click", roomEnter);
+		$("input[name=finish]").bind("click", roomEnter);
+		$("input[name=room]").keypress(roomCheck);
 		$("a.option").bind("click", BaseUI.optionSelect);
 	}
 	// Called after clicking "enter" on the room select form
@@ -24,8 +25,8 @@ var HomeView = (function() {
 			$("input[name=room]").effect("highlight", { color: "#FF0000" });
 			errors = true;
 		}
-		if($("input[name=color]").val() == "") {
-			$(".colors").effect("highlight", { color: "#FF0000" });
+		if($("input[name=color]").val() == "" || $("input[name=color]").length == 0) {
+			$(".selector").effect("highlight", { color: "#FF0000" });
 			errors = true;
 		}
 
@@ -41,10 +42,29 @@ var HomeView = (function() {
 		);
 	}
 
+	// Called on every keypress in the room name box
+	function roomCheck() {
+		clearInterval(check_timeout);
+		check_timeout = setTimeout(function() {
+			SyncServer.get(
+				"game/exists",
+				{ name: $("input[name=room]").val() },
+				roomCheckCb
+			);
+		}, 500);
+	}
+
+	function roomCheckCb(data) {
+		if(data['status'] == 200) {
+			$("input[name=finish]").removeClass("red").val("enter");
+		} else {
+			$("input[name=finish]").addClass("red").val("create room");
+		}
+	}
+
 	// Release public methods/vars
 	return {
 		"tpl": tpl,
 		"onLoad": onLoad,
-		"roomEnter":roomEnter,
 	};
 })();
