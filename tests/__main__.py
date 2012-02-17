98619f21f4efd243bf6e738ec10ff59e1fd8bec8
Steve Gattuso <stevenleeg@gmail.com>
2012-02-17 15:24:51 -0500

    Adds some basic unit testing!
    Oh my god this is my first time doing unit testing
    and I'm actually really excited about this stuff.
    Is that weird?

---
+++ tests/__main__.py
@@ -0,0 +1,4 @@
+import unittest
+from grid import *
+
+unittest.main()
---
+++ tests/grid.py
@@ -0,0 +1,35 @@
+from model import Grid, Coord
+from utility import db
+import unittest
+
+class TestGrid(unittest.TestCase):
+	def setUp(self):
+		db.flushdb()
+		self.g = Grid(1)
+	
+	def testCoordStorage(self):
+		c = self.g.get(0,5)
+		c['test'] = "Hello world" 
+		
+		self.assertEqual(c['test'], "Hello world")
+	
+	def testAround(self):
+		# Origin point
+		c = self.g.get(5,5)
+		c['test'] = "Hello"
+
+		# Create surrounding points
+		self.g.get(5,6)['test'] = True # Above
+		self.g.get(5,4)['test'] = True # Below
+		self.g.get(4,5)['test'] = True # Left
+		self.g.get(6,5)['test'] = True # Right
+
+		compare = ["5_6", "5_4", "4_5", "6_5"]
+		
+		around = self.g.around(c, 6)
+
+		for coord in compare:
+			self.assertIn(coord, around)
+	
+	def tearDown(self):
+		db.flushdb()
