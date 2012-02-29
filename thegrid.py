42bacea2091b0e0299ca7e365b385c5b5781a970
Steve Gattuso <stevenleeg@gmail.com>
2012-02-29 16:32:23 -0500

    Fixes spacing in thegrid.py

--- thegrid.py
+++ thegrid.py
@@ -10,28 +10,28 @@ import model.periodical
 
 # URL Routing
 app = tornado.web.Application([
-  # The API... Hereeee we go.
-  ('/api/grid/exists', api.grid.Exists),
-  ('/api/grid/create', api.grid.Create),
-  ('/api/grid/info', api.grid.Info),
+    # The API... Hereeee we go.
+    ('/api/grid/exists', api.grid.Exists),
+    ('/api/grid/create', api.grid.Create),
+    ('/api/grid/info', api.grid.Info),
 
-  ('/api/socket', controller.SocketHandler),
+    ('/api/socket', controller.SocketHandler),
 
-	("/static/(.*)", tornado.web.StaticFileHandler, {"path":"static"}),
-	("/", controller.Index),
+    ("/static/(.*)", tornado.web.StaticFileHandler, {"path":"static"}),
+    ("/", controller.Index),
 ], debug = True)
 
 if __name__ == "__main__":
-	app.listen(8080)
+    app.listen(8080)
 
-	# Thread for listener process
-	listener = threading.Thread(target = utility.UpdateManager.listen)
-	listener.daemon = True
-	listener.start()
+    # Thread for listener process
+    listener = threading.Thread(target = utility.UpdateManager.listen)
+    listener.daemon = True
+    listener.start()
 
-	loop = tornado.ioloop.IOLoop.instance()
-	tornado.ioloop.PeriodicCallback(model.periodical.payDay, 2500, loop).start()
-	tornado.ioloop.PeriodicCallback(model.periodical.infector, 5000, loop).start()
-	tornado.ioloop.PeriodicCallback(model.periodical.damager, 2000, loop).start()
+    loop = tornado.ioloop.IOLoop.instance()
+    tornado.ioloop.PeriodicCallback(model.periodical.payDay, 2500, loop).start()
+    tornado.ioloop.PeriodicCallback(model.periodical.infector, 5000, loop).start()
+    tornado.ioloop.PeriodicCallback(model.periodical.damager, 2000, loop).start()
 
-	loop.start()
+    loop.start()
