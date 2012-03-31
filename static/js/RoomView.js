fe993f91e0c592e2822c6c1e1224028a24825587
Steve Gattuso <stevenleeg@gmail.com>
2012-03-31 17:54:21 -0400

    Active players are now shown in the room UI

--- static/js/RoomView.js
+++ static/js/RoomView.js
@@ -6,11 +6,21 @@ var RoomView = (function() {
     }
 
     function joinRoomCb(data) {
+        // Load the game data
         GameData['pid'] = data['pid'];
         GameData['colors'] = data['colors'];
         GameData['color'] = GameData['colors'][GameData['pid']];
         GameData['active'] = data['active'];
         GameData['has_init'] = true;
+
+        // Update the UI
+        $.each(GameData['active'], function(i, pid) {
+            $("#p" + pid).css("background", GameData['colors'][pid]);
+        });
+
+        if(GameData['active'].length > 1) {
+            $("input[name=start]").removeClass("disabled");
+        }
     }
 
     return {
